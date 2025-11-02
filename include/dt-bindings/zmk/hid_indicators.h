bfb3f81ad465674bca0e4fc750003c8e28a025ec
Joel Spadin <joelspadin@gmail.com>
2025-11-01 22:00:16 -0500

    Define HID indicator bits using HID usage values
    Updated the HID_INDICATOR_* defines for devicetree bindings to be based
    on the HID_USAGE_LED_* defines instead of just being set to hexadecimal
    numbers. This doesn't necessarily make things more maintainable, since
    the descriptor in hid.h affects which bit represents each indicator,
    but it does make it clearer where these numbers come from.

--- include/dt-bindings/zmk/hid_indicators.h
+++ include/dt-bindings/zmk/hid_indicators.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#define HID_INDICATOR_NUM_LOCK 0x01
-#define HID_INDICATOR_CAPS_LOCK 0x02
-#define HID_INDICATOR_SCROLL_LOCK 0x04
-#define HID_INDICATOR_COMPOSE 0x08
-#define HID_INDICATOR_KANA 0x10
+#include <dt-bindings/zmk/hid_usage.h>
+
+// hid.h defines HID_USAGE_LED_NUM_LOCK as the minimum value, so that is bit
+// zero in the report, and all other indicators are relative to that.
+
+#define HID_INDICATOR_NUM_LOCK (1 << (HID_USAGE_LED_NUM_LOCK - HID_USAGE_LED_NUM_LOCK))
+#define HID_INDICATOR_CAPS_LOCK (1 << (HID_USAGE_LED_CAPS_LOCK - HID_USAGE_LED_NUM_LOCK))
+#define HID_INDICATOR_SCROLL_LOCK (1 << (HID_USAGE_LED_SCROLL_LOCK - HID_USAGE_LED_NUM_LOCK))
+#define HID_INDICATOR_COMPOSE (1 << (HID_USAGE_LED_COMPOSE - HID_USAGE_LED_NUM_LOCK))
+#define HID_INDICATOR_KANA (1 << (HID_USAGE_LED_KANA - HID_USAGE_LED_NUM_LOCK))
